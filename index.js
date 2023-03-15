595d73ea95b3c82444384fea6b17ad3c4415a222
cedric <cedricj765@gmail.com>
2023-03-15 16:06:18 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
-// Write your code here
+// Write your code
+
+let num1 = 2
+let num2 = 31
+
+let multiply = num1 * num2 
+
+
+let random = 1
+
+Number.isInteger = random 
+
+
+let num3 = 24
+let num4 = 5
+
+let mod = num3 % num4
+
+
+let max = (3, 4, 7, 20)
+
+
+
+
+
